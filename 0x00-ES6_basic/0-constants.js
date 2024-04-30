186355fd2823cbb664c6271187017cd33622cdb1
Shamainb <shamainbhengesa@gmail.com>
2024-04-30 11:48:50 +0000

    task0

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,33 +1,15 @@
 export function taskFirst() {
-<<<<<<< HEAD
-  var task = 'I prefer const when I can.';
-  return task;
+	const task = 'I prefer const when I can.';
+	return task;
 }
 
 export function getLast() {
-  return ' is okay';
+	return ' is okay';
 }
 
 export function taskNext() {
-  var combination = 'But sometimes let';
-  combination += getLast();
+	let combination = 'But sometimes let';
+	combination += getLast();
 
-  return combination;
+	return combination;
 }
-
-=======
-const task = 'I prefer const when I can.';
-return task;
-}
-
-export function getLast() {
-return ' is okay';
-}
-
-export function taskNext() {
-let combination = 'But sometimes let';
-combination += getLast();
-
-return combination;
-}
->>>>>>> cacecd341ba213d1cd75469924a2ea8fd4402056
