1e73ee9843628ae64a71bc7a23d4af64806d8f2d
Shamainb <shamainbhengesa60@gmail.com>
2024-05-03 22:24:40 +0200

    task1

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,5 @@
 function getListStudentIds(array) {
-{
-	if (!Array.isArray(array))
-		return [];
-}
-return array.map((a) => a.id);
+  if (!Array.isArray(array)) return [];
+  return array.map((a) => a.id);
 }
 export default getListStudentIds;
