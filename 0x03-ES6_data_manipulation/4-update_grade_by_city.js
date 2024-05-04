6f44cb5af4fc85d482b77294c64c47fb53c15696
Shamainb <shamainbhengesa60@gmail.com>
2024-05-04 12:03:36 +0200

    update

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,12 +2,11 @@ function updateStudentGradeByCity(students, city, newGrades) {
   return students
     .filter((e) => e.location === city)
     .map((e) => {
-      for (const grade of newGrades) {
-        if (grade === e.id) {
-          e.grade = grade.grade;
-        } else {
-          e.grade = 'N/A';
-        }
+      const grade = newGrades.find((grades) => grades.studentId === e.id);
+      if (grade) {
+        e.grade = grade.grade;
+      } else {
+        e.grade = 'N/A';
       }
       return e;
     });
