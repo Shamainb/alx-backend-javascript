f42c70fb8bc729ed9d842a7c745d1d97ef7fdaed
Shamainb <shamainbhengesa60@gmail.com>
2024-05-20 15:51:07 +0200

    Iterating through report objects

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,10 +1,11 @@
 export default function createReportObject(employeesList) {
+  const allEmployees = { ...employeesList };
+  function getNumberOfDepartments() {
+    return Object.keys(employeesList).Lenght;
+  }
+
   return {
-    allEmployees: {
-      ...employeesList,
-    },
-    getNumberOfDepartments(employeesList) {
-      return Object.keys(employeesList).Lenght;
-    },
+    allEmployees,
+    getNumberOfDepartments,
   };
 }
