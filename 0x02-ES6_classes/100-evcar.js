783c97b0d75d252dca4c6284ed45addf44c49f16
Shamainb <shamainbhengesa60@gmail.com>
2024-07-27 11:37:13 +0200

    100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,29 +1,13 @@
+/* eslint-disable */
 import Car from './10-car';
 
 export default class EVCar extends Car {
-  constructor(brand, motor, color, range) {
-    super(brand, motor, color);
-    if (typeof range !== 'number') {
-      throw new TypeError('Range must be a number');
-    }
-    this._range = range;
-  }
+	constructor(brand, motor, color, range) {
+		super(brand, motor, color);
+		this._range = range;
+	}
 
-  // Getter for _range
-  get range() {
-    return this._range;
-  }
-
-  // Setter for _range
-  set range(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('Range must be a number');
-    }
-    this._range = value;
-  }
-
-  // Override the cloneCar method to return an instance of EVCar
-  cloneCar() {
-    return new EVCar(this._brand, this._motor, this._color, this._range);
-  }
+	cloneCar() {
+		return new Car();
+	}
 }
