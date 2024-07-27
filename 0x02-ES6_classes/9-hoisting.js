545c74a5bc1115597101a98b0befd37f05d69ef3
Shamainb <shamainbhengesa60@gmail.com>
2024-07-27 11:29:33 +0200

    9-hoisting.js

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,11 +1,5 @@
 export class HolbertonClass {
   constructor(year, location) {
-    if (typeof year !== 'number') {
-      throw new TypeError('Year must be a number');
-    }
-    if (typeof location !== 'string') {
-      throw new TypeError('Location must be a string');
-    }
     this._year = year;
     this._location = location;
   }
@@ -17,40 +11,35 @@ export class HolbertonClass {
   get location() {
     return this._location;
   }
-
-  valueOf() {
-    return this._year;
-  }
-
-  toString() {
-    return this._location;
-  }
 }
+
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
-    if (typeof firstName !== 'string') {
-      throw new TypeError('First name must be a string');
-    }
-    if (typeof lastName !== 'string') {
-      throw new TypeError('Last name must be a string');
-    }
-    if (!(holbertonClass instanceof HolbertonClass)) {
-      throw new TypeError('holbertonClass must be an instance of HolbertonClass');
-    }
     this._firstName = firstName;
     this._lastName = lastName;
     this._holbertonClass = holbertonClass;
   }
 
-  get firstName() {
-    return this._firstName;
-  }
-
-  get lastName() {
-    return this._lastName;
+  get fullName() {
+    return `${this._firstName} ${this._lastName}`;
   }
 
   get holbertonClass() {
     return this._holbertonClass;
   }
+
+  get fullStudentDescription() {
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+  }
 }
+
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
+
+const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
+const student2 = new StudentHolberton('John', 'Doe', class2020);
+const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
+const student4 = new StudentHolberton('Donald', 'Bush', class2019);
+const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+
+export default [student1, student2, student3, student4, student5];
