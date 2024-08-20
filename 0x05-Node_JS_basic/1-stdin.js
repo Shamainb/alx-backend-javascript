e3fa0d6314c62121595f468bcce9edb6f5dcc86a
Shamainb <shamainbhengesa60@gmail.com>
2024-08-20 22:34:22 +0200

    stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,15 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+// Listen for user input
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  process.stdout.write(`Your name is: ${name}\n`);
+
+  // Exit the program after output
+  process.exit();
+});
+
+// Listen for the 'exit' event to display the closing message
+process.on('exit', () => {
+  console.log('This important software is now closing');
+});
